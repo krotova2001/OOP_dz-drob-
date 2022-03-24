60dbe88f8dd2ca09e2bca78db6d1bd70c53f4db2
Георгий <zvuk24@gmail.com>
2022-03-24 18:56:06 +0700

    1

--- Drob_class.h
+++ Drob_class.h
@@ -1,24 +1,17 @@
 #pragma once
+#include <iostream>
+
 class Drob
 {
 	int chis;
 	int znam;
 public:
-	Drob(int a, int b) : chis{a}, znam{b} // конструктор с параметрами и инициализацией
+	Drob(int a, int b) : chis{ a }, znam{ b } // конструктор с параметрами и инициализацией
 	{
 		if (b == 0) znam = 1; // проверка деления на ноль
-		cout << "\nError 0 division\n";
 	}
+	Drob():Drob(1,1){} // конструктор без параметров с делегированием на конструктор выше с параметрами по умолчанию
 	void Show();
-	
-	
-	/*
-	а эти операторы можно перегрузить
-	void Plus(Drob a); // сложение. Принимает вторую дробь и складывает с собой
-	void Minus(Drob a); // вычитание. Принимает вторую дробь и вычитает ее из себя
-	void Umnozh(Drob a); // умножение. Принимает вторую дробь и умножает ее на себя
-	void Delenie(Drob a); //деление. Принимает вторую дробь и делеит себя на нее
-	*/
 
 	Drob operator+(Drob a) // сложение
 	{
@@ -36,9 +29,50 @@ public:
 			// и складываем числители
 			chis += a.chis;
 		}
-		return 
+		return *this; // возвратим эту же дробь
 	}
 
+	Drob operator-(Drob a) // вычитание
+	{
+		if (a.znam == znam) //если знаменатели дробей равны - просто вычитаем числительные
+		{
+			chis -= a.chis;
+		}
+		else // если нет, приводим их к общему знаменателю
+		{
+			int znam_temp = znam; // возьмем "в уме" один знаменатель, так как на него неизмененного будем перемножать вторую дробь
+			chis *= a.znam;
+			znam *= a.znam;
+			a.chis *= znam_temp;
+			a.znam *= znam_temp;
+			// и вычитаем числители
+			chis -= a.chis;
+		}
+		return *this;
+	}
 
+	Drob operator*(Drob a) // умножение
+	{
+		chis *= a.chis; // перемножаем числители
+		znam *= a.znam; // и знаменатели
+		return *this;
+	}
+
+	Drob operator/(Drob a) // деление
+	{
+		chis *= a.znam;
+		znam *= a.chis;
+		return *this;
+	}
+
+	//хоть в таком простом классе и нет динамической памяти, реализуем конструктор копирования
+	Drob(const Drob& a):chis{ a.chis }, znam{ a.znam}{}
+
+	// а теперь можно и перегрузить оператор =
+	Drob operator=(Drob a)
+	{
+		chis = a.chis; 
+		znam = a.znam;
+		return *this;
+	}
 };
-
